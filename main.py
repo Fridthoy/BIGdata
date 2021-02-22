aa2ecb89159fed72f606b0bee0724834aa8dfde2
fridthoy <hoyer1996@hotmail.com>
2021-02-22 12:11:34 +0100

    firstcommit

---
+++ main.py
@@ -0,0 +1,51 @@
+from pyspark.sql import SparkSession
+
+
+spark = SparkSession.builder.appName("badgesLoad").getOrCreate()
+
+
+def returnBadges():
+    df = spark.read.csv(
+        path = 'badges.csv',
+        sep= "\t",
+        header= True,
+        quote='"',
+        schema="UserId INT, Name STRING, Date DATE, Class INT "
+    )
+    return df
+
+
+def returnComments():
+    df = spark.read.csv(
+        path = 'comments.csv',
+        sep= "\t",
+        header= True,
+        quote='"',
+        schema="PostId INT, Score INT, Text STRING, CreationDate DATE, UserId INT",
+    )
+    return df
+
+def returnPosts():
+    df = spark.read.csv(
+        path = 'posts.csv',
+        sep= "\t",
+        header= True,
+        quote='"',
+        inferSchema= True
+        #schema="PostId INT, Score INT, Text STRING, CreationDate DATE, UserId INT",
+    )
+    return df
+
+def returnUsers():
+    df = spark.read.csv(
+        path = 'users.csv',
+        sep= "\t",
+        header= True,
+        quote='"',
+        inferSchema= True
+        #schema="PostId INT, Score INT, Text STRING, CreationDate DATE, UserId INT",
+    )
+    return df
+
+if __name__ == '__main__':
+    returnUsers().show(20)
