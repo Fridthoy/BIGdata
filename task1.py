1cd17b7bb1003715d2da058590eecbe6808cedd6
Magnus Myklegard <mykle@dhcp-10-220-222-237.wlan.ntnu.no>
2021-02-23 11:59:29 +0100

    forbedringer

--- task1.py
+++ task1.py
@@ -1,30 +1,37 @@
-import findspark, os, shutil, csv
+from pyspark.sql import SparkSession
+import os
+import shutil
+import csv
+import findspark
 findspark.init()
 
-from pyspark.sql import SparkSession
 
 class Rdd:
 
     def init_spark(self, app_name="FirstApp", execution_mode="local[*]"):
-      spark = SparkSession.builder.master(execution_mode).appName(app_name).getOrCreate()
-      sc = spark.sparkContext
-      return spark, sc
-
+        spark = SparkSession.builder.master(
+            execution_mode).appName(app_name).getOrCreate()
+        sc = spark.sparkContext
+        return spark, sc
 
     def returnRddClass(self):
         _, sc = self.init_spark()
-        self.badges = sc.textFile('badges.csv').map(lambda element: element.split('\t'))
-        self.comments = sc.textFile('comments.csv')
-        self.posts = sc.textFile('posts.csv')
-        self.users = sc.textFile('users.csv').map(lambda element: element.split('\t'))
-
+        self.badges = sc.textFile('data/badges.csv').map(
+            lambda element: element.split('\t'))
+        self.comments = sc.textFile('data/comments.csv')
+        self.posts = sc.textFile('data/posts.csv')
+        self.users = sc.textFile('data/users.csv').map(
+            lambda element: element.split('\t'))
 
     def getBadges(self):
         return self.badges
+
     def getComments(self):
         return self.comments
+
     def getPosts(self):
         return self.posts
+
     def getusers(self):
         return self.users
 
@@ -35,11 +42,11 @@ def findNumberOfRows(rdd):
     print("Posts has ", rdd.getPosts().count(), " rows")
     print("Users has ", rdd.getusers().count(), " rows")
 
+
 if __name__ == '__main__':
     rdd = Rdd()
     rdd.returnRddClass()
 
-
-    #print(rdd.getComments().take(2))
+    # print(rdd.getComments().take(2))
 
     findNumberOfRows(rdd)
