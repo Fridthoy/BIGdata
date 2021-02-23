5559c63da503d2a9dd9ee75766fa4d197f5997a5
Magnus Myklegard <mykle@dhcp-10-220-222-237.wlan.ntnu.no>
2021-02-23 10:47:39 +0100

    old changes

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ spark = SparkSession.builder.appName("badgesLoad").getOrCreate()
 
 def returnBadges():
     df = spark.read.csv(
-        path = 'badges.csv',
-        sep= "\t",
-        header= True,
+        path='data/badges.csv',
+        sep="\t",
+        header=True,
         quote='"',
         schema="UserId INT, Name STRING, Date DATE, Class INT "
     )
@@ -18,35 +18,38 @@ def returnBadges():
 
 def returnComments():
     df = spark.read.csv(
-        path = 'comments.csv',
-        sep= "\t",
-        header= True,
+        path='data/comments.csv',
+        sep="\t",
+        header=True,
         quote='"',
         schema="PostId INT, Score INT, Text STRING, CreationDate DATE, UserId INT",
     )
     return df
 
+
 def returnPosts():
     df = spark.read.csv(
-        path = 'posts.csv',
-        sep= "\t",
-        header= True,
+        path='data/posts.csv',
+        sep="\t",
+        header=True,
         quote='"',
-        inferSchema= True
+        inferSchema=True
         #schema="PostId INT, Score INT, Text STRING, CreationDate DATE, UserId INT",
     )
     return df
 
+
 def returnUsers():
     df = spark.read.csv(
-        path = 'users.csv',
-        sep= "\t",
-        header= True,
+        path='data/users.csv',
+        sep="\t",
+        header=True,
         quote='"',
-        inferSchema= True
+        inferSchema=True
         #schema="PostId INT, Score INT, Text STRING, CreationDate DATE, UserId INT",
     )
     return df
+
 
 if __name__ == '__main__':
     returnUsers().show(20)
