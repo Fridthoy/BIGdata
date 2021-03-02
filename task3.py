72423bc1bc2f77b4a20fd51fa77dacfd21643ff4
Magnus Myklegard <mykle@dhcp-10-220-223-128.wlan.ntnu.no>
2021-03-02 10:51:28 +0100

    made extra changes

--- task3.py
+++ task3.py
@@ -9,7 +9,6 @@ from pyspark.sql import SparkSession, SQLContext
 from pyspark.sql import functions as func
 from pyspark.sql.window import Window
 
-#from pyspark.sql.functions import col, lit, when
 # Making a graph
 # Nodes = Users
 # Edges = comments
@@ -19,18 +18,15 @@ from pyspark.sql.window import Window
 #edges = sqlContext.createDataFrame('data/')
 
 
-def graph_of_posts_comments(postsrdd, commentsrdd, usersrdd):
-
-    nodes = usersrdd
+def graph_of_posts_comments(postsrdd, commentsrdd):
     posts = postsrdd
     comments = commentsrdd
-    #graph = posts.id
 
     # ------------ Joining the two RDDs ----------------
     joined_comments_and_posts = comments.join(posts).map(
-        lambda x: (x[1], 1)).reduceByKey(lambda a, b: a+b)
+        lambda columns: (columns[1], 1)).reduceByKey(lambda commentid, postid: commentid+postid)
     joined_comments_and_posts = joined_comments_and_posts.map(
-        lambda x: (x[0][0], x[0][1], x[1]))
+        lambda columns: (columns[0][0], columns[0][1], columns[1]))
     print(joined_comments_and_posts.take(10))
     return joined_comments_and_posts
 
@@ -71,6 +67,7 @@ if __name__ == '__main__':
     rdd = Rdd()
     rdd.returnRddClass()
     joined_RDD = graph_of_posts_comments(
-        rdd.getPosts(), rdd.getComments(), rdd.getusers())
+        rdd.getPosts(), rdd.getComments())
     rdd_data_frame = graphToDataframe(joined_RDD)
     # usersWithMostComments(rdd_data_frame)
+    # saveDF(joined_RDD)
